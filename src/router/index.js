e734b77f433552d5b544e9f544869dcbfc3ef04e
andoshin11 <shinglish11@gmail.com>
2018-04-12 01:54:40 +0900

    no vue-cli alias

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import TodoList from '@/components/TodoList'
+import Hello from '../components/Hello'
+import TodoList from '../components/TodoList'
 
 Vue.use(Router)
 
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: TodoList
     },
     {
       path: '/todo_list',
